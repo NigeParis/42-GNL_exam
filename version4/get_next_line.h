ae94fef0eeced83c39de4f47beb60e5dcdb93b94
Nigel Robinson <nrobinso@made-f0Cr13s4.clusters.42paris.fr>
2024-05-24 14:58:43 +0200

    deleted header not needed in .h

--- version4/get_next_line.h
+++ version4/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 09:05:56 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/05/24 09:10:28 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/05/24 14:57:22 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 
 char	*get_next_line(int fd);
